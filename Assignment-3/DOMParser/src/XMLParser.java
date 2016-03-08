9e30784b883956dcf4c625d64ebbdda5834f92c5
Stella <siddu7263@gmail.com>
2016-03-08 14:33:02 -0600

    * Updated Readme file * Added DOMParser * Added XML files * Added Corpus

---
+++ Assignment-3/DOMParser/src/XMLParser.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Sid19 on 3/7/2016.
+ */
+
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+
+public class XMLParser {
+    public static void main(String[] args)
+    {
+        try
+        {
+            File inputFile = new File("Cordova.xml"); //Enter File Name here.
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
+            NodeList nList = doc.getElementsByTagName("item");
+            System.out.println("\n------------------------------------------\n");
+            for (int temp = 0; temp < nList.getLength(); temp++)
+            {
+                Node nNode = nList.item(temp);
+                //System.out.println("\nCurrent Element: " + nNode.getNodeName());
+                if (nNode.getNodeType() == Node.ELEMENT_NODE)
+                {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Title: " + eElement.getElementsByTagName("title").item(0).getTextContent());
+                    System.out.println("Description: " + eElement.getElementsByTagName("description").item(0).getTextContent());
+                    //Comment out respective print statements to get either Title Alone, Description alone or both together
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
--- Assignment-3/Readme.md
+++ Assignment-3/Readme.md
@@ -1,1 +1,9 @@
 
+This Repository contains the Corpus for the following Software Repositories:
+ * Apache Spark
+ * Apache Cordova
+ * Apache HBASE
+
+ The Bug reports were taken in form of XML since Apache's JIRA system did not allow for the bug reports to be taken in 
+ the usual JSON format (Administrator Privileges were required). The XML files were parsed to get details such as the title,
+ description and both. These are hence included in the Corpus directory in this repository. 
